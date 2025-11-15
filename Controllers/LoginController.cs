0547b62bb365042c83edd4bac427bfddb6cf2fd3
Nick Burtner <cstrife@gmail.com>
2025-11-15 12:47:52 -0500

    login endpoint

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NEXUSDataLayerScaffold.Controllers
+{
+    [ApiController]
+    [Route("api/v1/login")]
+    public class LoginController : ControllerBase
+    {
+        // This endpoint is intentionally anonymous and simply redirects to '/'
+        // so external middleware can handle auth and bring the user back to the root.
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            // Use LocalRedirect to avoid open redirect vulnerabilities.
+            return LocalRedirect("/");
+        }
+    }
+}
