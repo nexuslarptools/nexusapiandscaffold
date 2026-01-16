d98b7542a0d7e5350e7bc5676a7c9d48481084c3
Nick Burtner <cstrife@gmail.com>
2026-01-16 13:11:54 -0500

    Testing with increased sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ echo "Starting SSH server..."
 /usr/sbin/sshd
 
 # Optional: Wait a few seconds for services to initialize.
-sleep 2
+sleep 3
 
 echo "Starting TestRemoteDebug application..."
 /otel-dotnet-auto/instrument.sh dotnet NEXUSDataLayerScaffold.dll --wait-for-debugger
