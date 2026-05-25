e8e75e66175e1f7e3f28005aea8537bb24c8c667
Brian Hykes <Brian.Hykes@gmail.com>
2026-05-25 14:54:00 -0400

    Allowing nulls on itemmultipick

--- Entities/ItemSheetInput.cs
+++ Entities/ItemSheetInput.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace NEXUSDataLayerScaffold.Entities;
 
 public class ItemSheetInput
 {
-    public List<Guid> A { get; set; }
-    public List<Guid> U { get; set; }
+    public List<Guid>? A { get; set; }
+    public List<Guid>? U { get; set; }
 }
